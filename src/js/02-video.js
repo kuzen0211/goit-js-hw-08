feb4044a871e434aa845f78904d73204baf3692a
Bohdan Bortnyk <bohdan0211@gmail.com>
2022-10-08 19:55:06 +0300

    Done 1,2,3 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const STORAGE_TIME = 'videoplayer-current-time';
+
+updateMainCurrentTime();
+
+const onPlay = function (currentTime) {
+  {
+    duration: 61.857;
+    percent: 0.049;
+    seconds: 3.034;
+  }
+  const currTime = currentTime.seconds;
+  console.log(currTime);
+  localStorage.setItem(STORAGE_TIME, currTime);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function updateMainCurrentTime() {
+  const saveTime = localStorage.getItem(STORAGE_TIME);
+
+  if (saveTime) {
+    player.setCurrentTime(saveTime);
+  }
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.input.addEventListener('input', throttle(onFormInput, 500));
+refs.textarea.addEventListener('input', throttle(onFormInput, 500));
+refs.form.addEventListener('input', onFormDataCollect);
+
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+insertData();
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  console.log(formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+  e.target.reset();
+}
+
+function onFormInput() {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function insertData() {
+  const saveData = localStorage.getItem(STORAGE_KEY);
+  const parseSaveData = JSON.parse(saveData);
+
+  if (saveData) {
+    refs.input.value = parseSaveData.email;
+    refs.textarea.value = parseSaveData.message;
+  }
+}
+
+function onFormDataCollect(e) {
+  formData[e.target.name] = e.target.value;
+}
